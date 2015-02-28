f59a7440d31e992f523b2e540f42283a5029565e
Urara Taguchi <urara.rarara.0312@gmail.com>
2015-02-28 17:55:18 +0900

    Initial Commit

---
+++ Clapbeat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clapbeat
+//
+//  Created by 田口うらら on 2015/02/28.
+//  Copyright (c) 2015年 田口うらら. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
